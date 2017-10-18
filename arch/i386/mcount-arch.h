2dd20e26ee3581e76bd41060adfbadf8b8458bd1
HanbumPark <kese111@gmail.com>
2017-10-17 18:59:07 -0700

    create arch/i386 edit mcount.S

---
+++ arch/i386/mcount-arch.h
@@ -0,0 +1,43 @@
+#ifndef __MCOUNT_ARCH_H__
+#define __MCOUNT_ARCH_H__
+
+#define mcount_regs  mcount_regs
+
+struct mcount_regs {
+	unsigned long  edx;
+	unsigned long  ecx;
+};
+
+#define  ARG1(a)  ((a)->ecx)
+#define  ARG2(a)  ((a)->edx)
+
+#define ARCH_MAX_REG_ARGS  2
+#define ARCH_MAX_FLOAT_REGS  8
+
+enum x86_reg_index {
+	X86_REG_INT_BASE = 0,
+	/* integer registers */
+	X86_REG_ECX,
+	X86_REG_EDX,
+
+	X86_REG_FLOAT_BASE = 100,
+	/* floating-point registers */
+	X86_REG_XMM0,
+	X86_REG_XMM1,
+	X86_REG_XMM2,
+	X86_REG_XMM3,
+	X86_REG_XMM4,
+	X86_REG_XMM5,
+	X86_REG_XMM6,
+	X86_REG_XMM7,
+};
+
+#define HAVE_MCOUNT_ARCH_CONTEXT
+struct mcount_arch_context {
+	double xmm[ARCH_MAX_FLOAT_REGS];
+};
+
+#define ARCH_PLT0_SIZE  16
+#define ARCH_PLTHOOK_ADDR_OFFSET  6
+
+#endif /* __MCOUNT_ARCH_H__ */
