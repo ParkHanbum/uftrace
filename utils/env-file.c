4d360969ba1da994fd95bf136219683188de651f
Hanbum Park <kese111@gmail.com>
2018-05-13 16:37:35 +0900

    utils: Add 'env-file' to support sharing environment based on file.
    by default, 'uftrace' use environment variable to share options with
    target program. because target program be forked from 'uftrace',
    target program can be share the environment variable. but this
    approach is no longer possible when use dynamic trace because there
    is no way to share environment variable between separated process.
    therefore, extracted command-line options into a file to share with
    the target program. 'env-file' contain some useful function to handle
    that file.
    
    Signed-off-by: Hanbum Park <kese111@gmail.com>

---
+++ utils/env-file.c
@@ -0,0 +1,88 @@
+#include "utils/env-file.h"
+
+int create_env_file(void)
+{
+	int fd;
+
+	fd = open(ENV_FILE, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd < 0)
+		pr_err_ns(ERROR_CREATE_FILE);
+
+	return fd;
+}
+
+
+int open_env_file(void)
+{
+	int fd;
+
+	fd = open(ENV_FILE, O_RDONLY);
+	if (fd < 0)
+		pr_err_ns(ERROR_OPEN_FILE);
+
+	return fd;
+}
+
+/*
+ * write a line to uftrace environment file with given argument fd.
+ * a line contain key-value pair with separator '='.
+ * like this,
+ *
+ * [example]
+ * UFTRACE_PLTHOOK=1
+ */
+void set_env_to_file(int fd, char *key, char *value)
+{
+	int res;
+	char buf[256];
+
+	sprintf(buf, "%s", key);
+	sprintf(buf + strlen(buf), "%s", "=");
+	sprintf(buf + strlen(buf), "%s\n", value);
+	res = write(fd, buf, strlen(buf));
+
+	if (res < 0)
+		pr_err_ns(ERROR_WHILE_WRITE);
+}
+
+void set_env_from_file(int fd)
+{
+	char buf[4096] = {0,};
+	char key[1024] = {0,};
+	char value[1024] = {0,};
+	uint32_t key_index = 0;
+	uint32_t value_index = 0;
+	bool flag = true;
+	int i, count;
+
+	count = read(fd, buf, 4096);
+	if (count < 0)
+		pr_err_ns(ERROR_READ_FILE);
+	close(fd);
+
+	for (i = 0; buf[i] != '\0' ; i++) {
+		if (buf[i] == '\n') {
+			pr_dbg("ENVIRONMENT\t%s : %s\n", key, value);
+			setenv(key, value, 1);
+			flag = true;
+			key_index = 0;
+			memset(key, 0, sizeof(key));
+			continue;
+		}
+		if (buf[i] == '=') {
+			if (flag) {
+				flag = false;
+				value_index = 0;
+				memset(value, 0, sizeof(value));
+				continue;
+			}
+		}
+
+		if (flag)
+			key[key_index++] = buf[i];
+		else
+			value[value_index++] = buf[i];
+	}
+}
+
+
